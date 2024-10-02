af5634e4e7a5968f514266a17a73c89697f83e44
Alizeh <alizehkhn04@gmail.com>
2024-10-02 15:44:45 -0400

    Change author and date

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -9,7 +9,9 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
- * @author dancye
+ * @author Konstantina
+ * @author Alizeh
+
  */
 public abstract class Card {
     //default modifier for child classes
